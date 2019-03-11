cd8fa668ae1e42869145868e16c825cd5bc80f7c
Junhui Chen <384673977@qq.com>
2019-03-11 19:37:32 +0800

    更换成springboot，beego太难用了

---
+++ server/src/main/java/com/nju171250/njuTeacher/NjuTeacherApplication.java
@@ -0,0 +1,13 @@
+package com.nju171250.njuTeacher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NjuTeacherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NjuTeacherApplication.class, args);
+	}
+
+}
