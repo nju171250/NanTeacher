3f307b93cf5b700c51671c4479a354f1d8948849
waterxjw <171250675@smail.nju.edu.cn>
2019-03-16 16:41:34 +0800

    是否点过赞

--- front/src/Comments.js
+++ front/src/Comments.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './Comments.css';
 import "./Config";
+import Axios from 'axios';
+import _ from 'lodash';
 import { BallScaleRippleMultiple } from 'react-pretty-loading';
 class Comments extends Component {
     constructor(props){
@@ -10,6 +12,63 @@ class Comments extends Component {
               comments:[]
           }
         }
+    }
+    
+    fetchFavouriteSituation(commentsId){
+      var favouriteInfos=[];
+      var data = {
+        openId: 'aaa',
+        commentIds: commentsId
+      };
+
+      
+      Axios.post(global.constants.baseUrl+"/getUserDoFavouriteSituation",data,
+      {headers:{
+        "Content-Type":"application/json; charset=UTF-8"
+      }})
+      .then(result=>{
+        
+        favouriteInfos=result.data;
+        console.log(this.state.comments)
+        var merge=_.map(this.state.data.comments,function(item){
+           var res=_.merge(item,_.find(favouriteInfos,{commentId:item.commentId}));
+           console.log(res)
+           return res;
+        });
+        console.log(merge)
+        this.setState({
+          data:{
+            comments:merge
+          }
+        })
+      })
+      .catch(function (error) {
+        console.log(error);
+      }); 
+      
+      
+    }
+    thumbsUp(commentId,likeIt){
+      let url=global.constants.baseUrl+"/thumbsUp?commentId="+commentId+"&likeIt="+likeIt+"&openid=aaa"
+      fetch(url)
+        .then(result => {
+           this.componentDidMount();
+          })
+        .catch(e => console.log(e));
+    }
+    getLikeCssStyle(favouriteSituation,type){
+      //favouriteSituation: true赞过 false踩过 null未点击
+      //type：true为赞 false为踩
+      const css={
+        'background-color': 'red'
+      }
+      console.log(favouriteSituation+" "+type)
+      if(favouriteSituation==type){
+        return css;
+      }
+      else{
+        return {};
+      }
     }
     componentDidMount(){
       this.setState({isFetching: true})
@@ -23,11 +82,13 @@ class Comments extends Component {
           }else{
             this.setState({data: {comments:result}, isFetching: false})
             this.props.onCommentsInit(this.state.data.comments.length);
+            this.fetchFavouriteSituation(result.map(p=>
+              p.commentId
+            ));
           }
           })
         .catch(e => console.log(e));
 
-      
     }
   render() {
       console.log(this.state)
@@ -44,11 +105,11 @@ class Comments extends Component {
                   <p className="commentTime">发布于{p.commentTime===null||p.commentTime.length<10?p.commentTime:p.commentTime.substring(0,10)}</p>
                   <p className="report">举报</p>
                   <div className="right">
-                   <div className="like">
+                   <div className="like" onClick={this.thumbsUp.bind(this,p.commentId,1)} style={this.getLikeCssStyle(p.favouriteSituation,true)}>
                      <i className="el-icon-caret-top"/>
                      <p className="likeNum">赞同{p.likeNum}</p>
                     </div>
-                   <div className="dislike">
+                   <div className="dislike" onClick={this.thumbsUp.bind(this,p.commentId,0)} style={this.getLikeCssStyle(p.favouriteSituation,false)}>
                     <i className="el-icon-caret-bottom"/>
                    </div>
                    </div>
