8384db80fb235657acf53082e32d109bd925333a
Junhui Chen <384673977@qq.com>
2019-03-17 12:39:18 +0800

    修改首页样式

--- front/src/Home.js
+++ front/src/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 //import './App.css';
 import {Link} from 'react-router-dom'
-
+import './Home.css'
 
 class Home extends Component {
     constructor(props){
